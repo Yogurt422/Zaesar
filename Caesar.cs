e361e88e8ed8509f40be63a77f8879d85597c84d
Yogurt422 <100674015+Yogurt422@users.noreply.github.com>
2022-03-01 11:01:17 +0300

    Add files via upload

---
+++ Caesar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._02._22
+{
+    internal class Caesar
+    {
+        private static string AlphabetRU =
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        public static string Encrypty(string input, int key)
+        {
+            input = input.ToUpper();
+
+            string output = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int inputIndex = AlphabetRU.IndexOf(input[i]);
+                if (inputIndex != -1)
+                {
+                    output += AlphabetRU[(inputIndex + key) % AlphabetRU.Length];
+                }
+                else
+                    output += input[i];
+
+            }
+            return output;
+        }
+        public static string Decrypt(string input, int key)
+        {
+            input = input.ToUpper();
+
+            string output = string.Empty;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int inputIndex = AlphabetRU.IndexOf(input[i]);
+                if (inputIndex != -1)
+                {
+                    output += AlphabetRU[(AlphabetRU.Length + inputIndex - key)
+                            % AlphabetRU.Length];
+                    
+                   
+                }
+                else
+                    output += input[i];
+
+            }
+            return output;
+        }
+    }
+}
